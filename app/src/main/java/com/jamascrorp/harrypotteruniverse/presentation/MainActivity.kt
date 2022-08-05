ffc9889c9252602ad5101f8e40d6b6b9066fd22e
scorp97 <jamkapanamka97@gmail.com>
2022-08-05 16:18:15 +0500

    First Impl

--- app/src/main/java/com/jamascrorp/harrypotteruniverse/presentation/MainActivity.kt
+++ app/src/main/java/com/jamascrorp/harrypotteruniverse/presentation/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.jamascrorp.harrypotteruniverse.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.jamascrorp.harrypotteruniverse.R
+import com.jamascrorp.harrypotteruniverse.data.GameUseCaseRepositoryImpl
+import com.jamascrorp.harrypotteruniverse.presentation.FragmentMain.MainFragment
+import com.jamascrorp.harrypotteruniverse.presentation.FragmentResult.ResultFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
