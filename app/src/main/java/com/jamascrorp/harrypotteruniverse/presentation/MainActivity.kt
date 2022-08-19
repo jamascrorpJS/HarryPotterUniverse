860bc7829ae45a8a3b7a858f164e28fed5e94afd
jamascrorpJS <jama1997@mail.ru>
2022-08-19 11:14:58 +0500

    Finish App

--- app/src/main/java/com/jamascrorp/harrypotteruniverse/presentation/MainActivity.kt
+++ app/src/main/java/com/jamascrorp/harrypotteruniverse/presentation/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.jamascrorp.harrypotteruniverse.presentation
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.jamascrorp.harrypotteruniverse.R
-import com.jamascrorp.harrypotteruniverse.data.GameUseCaseRepositoryImpl
-import com.jamascrorp.harrypotteruniverse.presentation.FragmentMain.MainFragment
-import com.jamascrorp.harrypotteruniverse.presentation.FragmentResult.ResultFragment
 
 class MainActivity : AppCompatActivity() {
 
